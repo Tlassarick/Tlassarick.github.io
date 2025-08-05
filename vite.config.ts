8311747339f1cbaea7c7b769c74c504daa60cad4
Terroros <leshagulevych1@gmail.com>
2025-08-05 15:13:29 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], base: '/Tlassarick.github.io'
+  plugins: [react()], 
+  base: '/tlassarick.github.io/'
 })
